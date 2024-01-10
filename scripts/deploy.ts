a45a7a684bc3e777312cc68da4d93306e3cd4af4
PSheon <pauljiang61020@gmail.com>
2024-01-10 15:44:49 +0800

    feat: added deploy script.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,17 @@
-import { formatEther, parseEther } from "viem";
 import hre from "hardhat";
 
+const MAX_BATCH_SIZE = 5;
+const COLLECTION_SIZE = 5000;
+const AMOUNT_FOR_DEV = 15;
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = BigInt(currentTimestampInSeconds + 60);
+  const mainContract = await hre.viem.deployContract("NFT", [
+    MAX_BATCH_SIZE,
+    COLLECTION_SIZE,
+    AMOUNT_FOR_DEV,
+  ]);
 
-  const lockedAmount = parseEther("0.001");
-
-  const lock = await hre.viem.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  console.log(
-    `Lock with ${formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`Deployed to ${mainContract.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
